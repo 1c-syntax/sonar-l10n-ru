2287b0146068076a344cf03bfefbd24408bc4e7f
Alexey Lustin <alustin@silverbulleters.org>
2016-08-09 08:41:52 +0300

    change docker image to latest alpine based on SonarSource/docker-sonarqube#40

--- rebuild-and-restart.sh
+++ rebuild-and-restart.sh
@@ -4,6 +4,6 @@ echo "on MSWIN use msgit with linux extension sed,grep and others"
 
 mvn clean package -Dlicense.skip=true
  
-docker run -d --name=sq-develop -p 9000:9000 sonarqube:5.6.1-alpine || docker stop sq-develop
+docker run -d --name=sq-develop -p 9000:9000 sonarqube:alpine || docker stop sq-develop
 docker cp ./target/*.jar sq-develop:/opt/sonarqube/extensions/plugins/
 docker start sq-develop
