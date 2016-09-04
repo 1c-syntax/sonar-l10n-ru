22b3c8bbf7a2d0298e262bd9b274095d73f0adb5
Nikita Gryzlov <nixel2007@gmail.com>
2016-09-04 13:16:05 +0300

    Bump version to 6.0

--- rebuild-and-restart.sh
+++ rebuild-and-restart.sh
@@ -3,7 +3,7 @@ echo "use maven in your host system with PATH support"
 echo "on MSWIN use msgit with linux extension sed,grep and others"
 
 mvn clean package -Dlicense.skip=true
- 
-docker run -d --name=sq-develop -p 9000:9000 sonarqube:5.6.1-alpine || docker stop sq-develop
+
+docker run -d --name=sq-develop -p 9000:9000 sonarqube:6.0-alpine || docker stop sq-develop
 docker cp ./target/*.jar sq-develop:/opt/sonarqube/extensions/plugins/
 docker start sq-develop
