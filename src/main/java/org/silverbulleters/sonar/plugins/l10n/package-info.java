e8535570653a5ebef4cce9366b019c8066232f9a
theshadowco <maximovvalery@gmail.com>
2023-03-03 14:26:28 +0300

    add pack-info

---
+++ src/main/java/org/silverbulleters/sonar/plugins/l10n/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * This file is a part of Russian Pack for SonarQube.
+ *
+ * Copyright (c) 2016-2018
+ * SilverBulleters, LLC
+ * mailto:b2b@silverbulleters.org
+ *
+ * Copyright (c) 2020-2023
+ * 1c-syntax and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * Russian Pack for SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * Russian Pack for SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Russian Pack for SonarQube.
+ */
+/**
+ * Здесь вся реализация плагина
+ */
+package org.silverbulleters.sonar.plugins.l10n;
