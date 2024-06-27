3f851bf94c7adcd51a791251f5c46548ef34554b
Maximov Valery <maximovvalery@gmail.com>
2024-06-27 07:53:54 +0000

    fix comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ gitVersioning.apply(closureOf<GitVersioningPluginConfig> {
 })
 
 val sonarQubeVersion = "10.5.1.90531"
-val sonarQubeAPIPluginVersion = "10.6.0.2114" # https://github.com/SonarSource/sonar-plugin-api#compatibility
+val sonarQubeAPIPluginVersion = "10.6.0.2114" // https://github.com/SonarSource/sonar-plugin-api#compatibility
 val junitVersion = "5.6.1"
 
 dependencies {
