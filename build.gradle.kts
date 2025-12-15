32173c60204303db13852a1cf3c7a296d41412d4
Maximov Valery <maximovvalery@gmail.com>
2025-12-15 19:49:38 +0300

    up gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ gitVersioning.apply {
 // https://github.com/SonarSource/sonarqube/blob/master/gradle.properties
 val sonarQubeVersion = "25.11.0.114957"
 val sonarQubeAPIPluginVersion = "13.0.0.3026" // https://github.com/SonarSource/sonar-plugin-api#compatibility
-val junitVersion = "5.6.1"
 
 dependencies {
     compileOnly("org.sonarsource.api.plugin", "sonar-plugin-api", sonarQubeAPIPluginVersion)
@@ -209,6 +208,9 @@ sonarqube {
         property("sonar.organization", "1c-syntax")
         property("sonar.projectKey", "1c-syntax_sonar-l10n-ru")
         property("sonar.projectName", "Russian Pack for SonarQube")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/test/jacoco.xml")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${layout.buildDirectory.get()}/reports/jacoco/test/jacoco.xml"
+        )
     }
 }
